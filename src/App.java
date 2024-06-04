e0689c26a721eb4a63386064e7b5b1dc16a37908
PedroHCurioni <pedrohcurioni21@gmail.com>
2024-06-03 21:30:05 -0300

    Adicionando Subtração

--- src/App.java
+++ src/App.java
@@ -6,6 +6,9 @@ public class App {
         switch (operador){                 
             case "+":
                 return num1 + num2;
+                case "-":
+                return num1 - num2;
+
         default:
             return 0;
         }
